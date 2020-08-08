c1cbb4a4e4ee6d2efbf2ee93090f90d63e030a4c
anshbest <anshulmnc123@gmail.com>
2020-08-09 02:34:11 +0530

    stored each operation value in dictionary

--- arithmeticcalculation.sh
+++ arithmeticcalculation.sh
@@ -4,7 +4,14 @@ read -p "Enter number a: " a
 read -p "Enter number b: " b
 read -p "Enter number c: " c
 
+declare -A opdict
 op1=$(($a+$b*$c))
 op2=$(($a*$b+$c))
 op3=$(($a+$b/$c))
 op4=$(($a%$b+$c))
+for (( index=1 ; index<=4 ; index++ ))
+do
+	opdict["Op""$index"]=$((op$index))
+         echo ${opdict[@]}
+         echo key  ${!opdict[@]}
+done
