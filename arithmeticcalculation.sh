f825006895420df91c561de67f862264cbe4f16e
anshbest <anshulmnc123@gmail.com>
2020-08-09 03:06:34 +0530

    Adding values in array from  dictionary

--- arithmeticcalculation.sh
+++ arithmeticcalculation.sh
@@ -5,13 +5,22 @@ read -p "Enter number b: " b
 read -p "Enter number c: " c
 
 declare -A opdict
+declare -a opArray
 op1=$(($a+$b*$c))
 op2=$(($a*$b+$c))
 op3=$(($a+$b/$c))
 op4=$(($a%$b+$c))
 for (( index=1 ; index<=4 ; index++ ))
 do
-	opdict["Op""$index"]=$((op$index))
+	opdict[$index]=$((op$index))
          echo ${opdict[@]}
          echo key  ${!opdict[@]}
+done
+
+
+for (( j=0 ; j<4 ; j++ ))
+do
+        opArray[$j]=${opdict[$(($j+1))]}
+         echo "${opArray[@]}"
+         echo Array Index  ${!opArray[@]}
 done
